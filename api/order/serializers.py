aa7ff0fba92dde7da09cbe7a2342e4536b4f421b
pc5 <pc5@bankcomparebd.com>
2020-08-19 13:15:21 +0600

    Package Total Pirce

--- api/order/serializers.py
+++ api/order/serializers.py
@@ -1,15 +1,24 @@
 from rest_framework import serializers
 
+from api.package.serializers import PackageSerializer
 from apps.sales.models import Order, OrderDetails
 
 
-class OrderSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Order
-        fields = '__all__'
-
 class OrderDetailsSerializer(serializers.ModelSerializer):
+    package_details = PackageSerializer(source='package', read_only=True)
+
     class Meta:
         model = OrderDetails
         fields = '__all__'
 
+
+class OrderSerializer(serializers.ModelSerializer):
+    order_details = serializers.SerializerMethodField(read_only=True)
+
+    def get_order_details(self, obj):
+        order_details = OrderDetails.objects.filter(order=obj)
+        return OrderDetailsSerializer(order_details, many=True).data
+
+    class Meta:
+        model = Order
+        fields = '__all__'
