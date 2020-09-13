e66055171214e454c092afee47205abdfcd77341
pc5 <pc5@bankcomparebd.com>
2020-09-13 14:11:51 +0600

    Zone Wise PDF

--- apps/packages/models.py
+++ apps/packages/models.py
@@ -9,8 +9,8 @@ from apps.products.models import AgeGroup, NutritionPoint, Product
 class Package(TimeStamp, OperatorStamp):
     name = models.CharField(max_length=255)
     size = models.IntegerField(choices=SIZE, default=1)
-    age_group = models.ForeignKey(AgeGroup, on_delete=models.CASCADE)
-    nutrition_point = models.ForeignKey(NutritionPoint, on_delete=models.CASCADE,verbose_name='Cure Disease Package')
+    age_group = models.ForeignKey(AgeGroup, on_delete=models.SET_NULL,null=True,blank=True)
+    nutrition_point = models.ForeignKey(NutritionPoint, on_delete=models.SET_NULL,null=True,blank=True,verbose_name='Cure Disease Package')
     nutrition_details = models.TextField()
     discount_percentage = models.FloatField(default=0)
     suggestion = models.IntegerField(default=10)
