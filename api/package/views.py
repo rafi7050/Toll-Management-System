e4e6ddab8959fe25ffc01ba321eff12f75c33173
pc5 <pc5@bankcomparebd.com>
2020-09-13 14:41:38 +0600

    Age Group Nutrition Point and Size Wise Filter

--- api/package/views.py
+++ api/package/views.py
@@ -1,7 +1,9 @@
+from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework import viewsets
 from rest_framework.authentication import TokenAuthentication
 from rest_framework.permissions import IsAuthenticated
 from rest_framework.response import Response
+from rest_framework_datatables.filters import DatatablesFilterBackend
 
 from api.package.serializers import PackageSerializer
 from apps.helpers.utils import SIZE
@@ -14,21 +16,28 @@ class PackageViewSet(viewsets.ModelViewSet):
     queryset = Package.objects.filter(package_type=1)
     serializer_class = PackageSerializer
 
-    def get_queryset(self):
-        nutrition_point = self.request.GET.get('nutrition_point', None)
-        age_group = self.request.GET.get('age_group', None)
-        size = self.request.GET.get('size', None)
+    filter_backends = (DatatablesFilterBackend, DjangoFilterBackend,)
+    filter_fields = {
+        'nutrition_point': ['exact'],
+        'age_group': ['exact'],
+        'size': ['exact'],
+    }
 
-        if nutrition_point:
-            self.queryset = self.queryset.filter(nutrition_point=nutrition_point)
-
-        if age_group:
-            self.queryset = self.queryset.filter(age_group=age_group)
-
-        if size:
-            self.queryset = self.queryset.filter(size=size)
-
-        return self.queryset
+    # def get_queryset(self):
+    #     nutrition_point = self.request.GET.get('nutrition_point', None)
+    #     age_group = self.request.GET.get('age_group', None)
+    #     size = self.request.GET.get('size', None)
+    #
+    #     if nutrition_point:
+    #         self.queryset = self.queryset.filter(nutrition_point=nutrition_point)
+    #
+    #     if age_group:
+    #         self.queryset = self.queryset.filter(age_group=age_group)
+    #
+    #     if size:
+    #         self.queryset = self.queryset.filter(size=size)
+    #
+    #     return self.queryset
 
 
 class ClientPackageViewSet(viewsets.ModelViewSet):
