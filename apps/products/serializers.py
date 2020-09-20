7163afbf8e2d3a293a4c7b8c8bbf73924aad8e31
pc5 <pc5@bankcomparebd.com>
2020-09-20 22:05:34 +0600

    Product Type Done

--- apps/products/serializers.py
+++ apps/products/serializers.py
@@ -4,9 +4,18 @@ from apps.products.models import Product, AgeGroup, NutritionPoint, ProductType
 
 
 class ProductSerializer(serializers.ModelSerializer):
+    product_type_name = serializers.SerializerMethodField(read_only=True)
+
+    def get_product_type_name(self, obj):
+        try:
+            return obj.product_type.name
+        except:
+            pass
+
     class Meta:
         model = Product
         fields = '__all__'
+
 
 class AgeGroupSerializer(serializers.ModelSerializer):
     class Meta:
@@ -24,4 +33,3 @@ class ProductTypeSerializer(serializers.ModelSerializer):
     class Meta:
         model = ProductType
         fields = '__all__'
-
