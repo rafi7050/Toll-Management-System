978c459a2ae01fa56d065d98902fcfa9279a7330
pc5 <pc5@bankcomparebd.com>
2020-10-01 05:57:50 +0600

    Set Default For Mail

--- pricing/settings.py
+++ pricing/settings.py
@@ -189,14 +189,14 @@ CORS_ORIGIN_ALLOW_ALL = True
 # EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
 #
 
-#
-# EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
-# EMAIL_HOST = config('EMAIL_HOST', default='')
-# EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
-# EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
-# EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
-# EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
-#
-# DEFAULT_FROM_EMAIL = 'DailyShobji <support@dailyshobji.com>'
-#
-# EMAIL_SUBJECT_PREFIX = '[DailyShobji]'
+
+EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
+EMAIL_HOST = config('EMAIL_HOST', default='')
+EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
+EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
+EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
+EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
+
+DEFAULT_FROM_EMAIL = 'DailyShobji <support@dailyshobji.com>'
+
+EMAIL_SUBJECT_PREFIX = '[DailyShobji]'
