fabb17a7d9637deb02710cfe6c70fcdb3f2fa760
pc5 <pc5@bankcomparebd.com>
2020-10-01 07:20:15 +0600

    Set Default For Mail

--- pricing/settings.py
+++ pricing/settings.py
@@ -189,7 +189,6 @@ CORS_ORIGIN_ALLOW_ALL = True
 # EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
 #
 
-
 EMAIL_BACKEND = config('EMAIL_BACKEND', default='django.core.mail.backends.smtp.EmailBackend')
 EMAIL_HOST = config('EMAIL_HOST', default='')
 EMAIL_PORT = config('EMAIL_PORT', default=587, cast=int)
@@ -197,6 +196,6 @@ EMAIL_HOST_USER = config('EMAIL_HOST_USER', default='')
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD', default='')
 EMAIL_USE_TLS = config('EMAIL_USE_TLS', default=True, cast=bool)
 
-DEFAULT_FROM_EMAIL = 'DailyShobji <support@aamartaka.com>'
+DEFAULT_FROM_EMAIL = 'Aamartaka <support@aamartaka.com>'
 
 EMAIL_SUBJECT_PREFIX = '[DailyShobji] '
