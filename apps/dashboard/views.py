b9c3d665a08e0ae0276116da7a1332fe5367064a
pc5 <pc5@bankcomparebd.com>
2020-10-07 00:03:29 +0600

    Dashboard

--- apps/dashboard/views.py
+++ apps/dashboard/views.py
@@ -10,6 +10,10 @@ import datetime
 from calendar import monthrange
 from django.contrib.auth.models import Group, User
 
+from apps.accounts.models import UserProfile
+from apps.packages.models import Package
+from apps.products.models import Product
+from apps.sales.models import Order
 
 PRODUCT_TYPE = (
     (1, 'Credit Card'),
@@ -26,3 +30,12 @@ PRODUCT_TYPE_WOORI = (
 
 class DashboardView(LoginRequiredMixin, TemplateView):
     template_name = 'dashboard/home.html'
+
+    def get_context_data(self, **kwargs):
+        context = super().get_context_data(**kwargs)
+        context['Orders'] = Order.objects.count()
+        context['Products'] = Product.objects.count()
+        context['Packages'] = Package.objects.count()
+        context['Customers'] = UserProfile.objects.filter(user_type=2).count()
+
+        return context
